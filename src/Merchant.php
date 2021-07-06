d1682e5166e682277e9f2ebc16cd01aff2efee02
akhur <akhur02@mail.ru>
2021-07-06 11:25:30 +0500

    возврат урла

--- src/Merchant.php
+++ src/Merchant.php
@@ -78,7 +78,7 @@ class Merchant extends BaseObject
         $invoice = AlfapayInvoice::findOne(['related_id' => $orderID . '-' . $this->getNumberSuffix(), 'related_model' => $relatedModel]);
 
         if ($invoice) {
-            return Yii::$app->response->redirect($invoice->url);
+            return $invoice->url;
         }
 
         $data = [
